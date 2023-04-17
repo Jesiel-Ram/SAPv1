c9470ef63442d5ed6e53934dc1cb966b44670a92
Jesiel Ram <neafzm3r@gmail.com>
2023-04-17 16:58:15 -0600

    Se agrega Datos.java el cual ya contiene los parametros

--- src/main/java/UX/Datos.java
+++ src/main/java/UX/Datos.java
@@ -1,4 +1,117 @@
 package UX;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.SystemColor;
+import javax.swing.JTextField;
+
 public class Datos {
+
+    private JFrame frame;
+    private JTextField numParte;
+    private JTextField descripcion;
+    private JTextField pvb;
+    private JTextField cantidad;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Datos window = new Datos();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public Datos() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+        frame = new JFrame();
+        frame.setResizable(false);
+        frame.setBounds(100, 100, 451, 613);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(null);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(SystemColor.activeCaption);
+        panel.setBounds(0, 0, 445, 573);
+        frame.getContentPane().add(panel);
+        panel.setLayout(null);
+
+        JLabel lblNewLabel = new JLabel("No. Parte");
+        lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 27));
+        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        lblNewLabel.setBounds(1, 92, 445, 33);
+        panel.add(lblNewLabel);
+
+        JLabel lblDescripcin = new JLabel("Descripci\u00F3n");
+        lblDescripcin.setHorizontalAlignment(SwingConstants.CENTER);
+        lblDescripcin.setFont(new Font("Tahoma", Font.PLAIN, 27));
+        lblDescripcin.setBounds(2, 188, 445, 33);
+        panel.add(lblDescripcin);
+
+        JLabel lblPvb = new JLabel("PVB");
+        lblPvb.setHorizontalAlignment(SwingConstants.CENTER);
+        lblPvb.setFont(new Font("Tahoma", Font.PLAIN, 27));
+        lblPvb.setBounds(0, 297, 445, 33);
+        panel.add(lblPvb);
+
+        JLabel lblCantSolicitada = new JLabel("Cant. Solicitada");
+        lblCantSolicitada.setHorizontalAlignment(SwingConstants.CENTER);
+        lblCantSolicitada.setFont(new Font("Tahoma", Font.PLAIN, 27));
+        lblCantSolicitada.setBounds(0, 398, 445, 33);
+        panel.add(lblCantSolicitada);
+
+        JLabel lblPvb_1 = new JLabel("");
+        lblPvb_1.setHorizontalAlignment(SwingConstants.CENTER);
+        lblPvb_1.setFont(new Font("Tahoma", Font.PLAIN, 27));
+        lblPvb_1.setBounds(1, 405, 445, 33);
+        panel.add(lblPvb_1);
+
+        JLabel lblSapStock = new JLabel("SAP STOCK");
+        lblSapStock.setHorizontalAlignment(SwingConstants.CENTER);
+        lblSapStock.setFont(new Font("Tahoma", Font.ITALIC, 30));
+        lblSapStock.setBounds(1, 1, 445, 33);
+        panel.add(lblSapStock);
+
+        numParte = new JTextField();
+        numParte.setBounds(154, 147, 146, 26);
+        panel.add(numParte);
+        numParte.setColumns(10);
+
+        descripcion = new JTextField();
+        descripcion.setColumns(10);
+        descripcion.setBounds(154, 246, 146, 26);
+        panel.add(descripcion);
+
+        pvb = new JTextField();
+        pvb.setColumns(10);
+        pvb.setBounds(154, 346, 146, 26);
+        panel.add(pvb);
+
+        cantidad = new JTextField();
+        cantidad.setColumns(10);
+        cantidad.setBounds(154, 456, 146, 26);
+        panel.add(cantidad);
+    }
+
 }
--- src/main/java/UX/Loggeo.java
+++ src/main/java/UX/Loggeo.java
@@ -1,4 +1,151 @@
 package UX;
 
-public class Loggeo {
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.net.MalformedURLException;
+
+//import javax.swing.JButton;
+
+public class Loggeo extends JFrame {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+    Color myColor = new Color(0, 255, 214);
+    Color fuera = new Color(12, 199, 232);
+    JTextField usuario = new JTextField();
+    JLabel grammer = new JLabel();
+    JLabel logo = new JLabel();
+    JFrame regisFrame;
+    boolean cerrarFrame;
+    String nUser = "";
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Loggeo frame = new Loggeo();
+                    frame.setVisible(true);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public Loggeo() {
+        setTitle("SAPV5");
+
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 366, 517);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JPanel panelFuera = new JPanel();
+        panelFuera.setBounds(0, 0, 360, 488);
+        panelFuera.setBackground(fuera);
+        contentPane.add(panelFuera);
+        panelFuera.setLayout(null);
+
+        grammer.setVerticalAlignment(SwingConstants.TOP);
+        grammer.setHorizontalAlignment(SwingConstants.CENTER);
+        grammer.setLocation(10,21);
+        grammer.setSize(323, 162);
+        logo.setLocation(128, 437);
+
+
+        JPanel panelDentro = new JPanel();
+        panelDentro.setBounds(49, 162, 255, 255);
+        panelDentro.setBackground(new Color(175, 238, 238));
+        panelFuera.add(panelDentro);
+        panelDentro.setLayout(null);
+
+        panelFuera.add(grammer);
+
+        usuario.setBounds(60, 80, 142, 30);
+        usuario.setText(usuario.getText().toUpperCase());
+        panelDentro.add(usuario);
+        usuario.setBackground(Color.WHITE);
+
+
+        JLabel lblBienvenido = new JLabel("BIENVENIDO");
+        lblBienvenido.setForeground(new Color(0, 0, 51));
+        lblBienvenido.setBounds(0, 0, 255, 52);
+        panelDentro.add(lblBienvenido);
+        lblBienvenido.setFont(new Font("Goudy Old Style", Font.PLAIN, 25));
+        lblBienvenido.setHorizontalAlignment(SwingConstants.CENTER);
+
+        Action actionUser = new AbstractAction() {
+            private static final long serialVersionUID = 1L;
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+
+                nUser = usuario.getText().toUpperCase();
+                System.out.println(nUser);
+                try {
+
+                    if(null != regisFrame && cerrarFrame) {
+                        regisFrame.setVisible(false);
+                    }
+
+                }catch(Exception e1) {
+                    System.out.println("Error no entramos");
+                    e1.printStackTrace();
+                }
+            }
+        };
+
+        usuario.addActionListener(actionUser);
+        usuario.addKeyListener(new KeyListener() {
+
+            public void keyTyped(KeyEvent evt) {
+
+            }
+
+            public void keyPressed(KeyEvent arg0) {
+
+            }
+
+            public void keyReleased (KeyEvent arg0) {
+
+            }
+
+        });
+
+
+    }
 }
